74380b57bc0d9c1e3d668f1318a33a87b210ff59
Olabenjo Tunde <t.visualb@hotmail.com>
2014-10-11 12:16:14 +0300

    Created services file for AnonChat

--- src/gui/GuiListener.java
+++
@@ -1,7 +0,0 @@
-package gui;
-
-import core.Buddy;
-
-public interface GuiListener {
-	public String onCommand(Buddy buddy, String s);
-}
--- src/listeners/CommandListener.java
+++
@@ -1,7 +0,0 @@
-package listeners;
-
-import core.Buddy;
-
-public interface CommandListener {
-	public void onCommand(Buddy buddy, String s);
-}
